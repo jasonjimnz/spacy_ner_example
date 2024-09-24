965cf5446a94535c1c8da70547f5d286c5d361a8
Jason Jiménez Cruz <jasonjimenezcruz@gmail.com>
2024-09-24 21:31:28 +0200

    Added required files for article experiments

---
+++ README.md
@@ -0,0 +1,15 @@
+# Streamlit example
+
+This is a repo for start playing with Spacy adn NER Training
+
+## Instructions
+
+- You will need to run under python 3.8+ (tested on python 3.11)
+- You can run it using the Docker file
+- Copy the `nlp_app_esp.py` file for adding new functionalities
+
+## Important
+This repo refers to an article written in Spanish, if you consider 
+that it requires english translation let me know in Medium
+
+Link to the article: [https://medium.com/@jasonjimenezcruz/spacy-acercamiento-al-procesamiento-del-lenguaje-natural-203cf73498c9](https://medium.com/@jasonjimenezcruz/spacy-acercamiento-al-procesamiento-del-lenguaje-natural-203cf73498c9)
---
+++ nlp_app_esp.py
@@ -0,0 +1,66 @@
+import spacy
+import random
+from spacy.training import Example
+from typing import Union, Dict, List
+
+LANGUAGE = 'es_core_news_sm'
+TEXT = "Este verano, quiero ir a visitar la Alhambra"
+TRAINING_TEXTS = [
+    (
+        # Texto que añadiremos como entrenamiento
+        "En Granada se encuentra situada la Alhambra",
+        # Lista de tuplas que contiene: Posición de inicio de la etiqueta, posición final, y nombre de la etiqueta
+        [(35, 43, "lugar_de_interes")]
+    ),
+    (
+        "El último fin de semana quisimos visitar el Alcázar de Segovia, pero el mal tiempo nos hizo posponerlo",
+        [(44, 51, "lugar_de_interes")]
+    ),
+    (
+        "Queríamos ir a Cuenca, pero se nos estropeó el coche y no pudimos visitar las Casas Colgadas, iremos el mes que viene",
+        [(78, 92, "lugar_de_inteeres")]
+    ),
+    (
+        "Las construcción de la Sagrada Familia está alargándose demasiado, a ver cuando la terminan para ir a Barcelona",
+        [(23, 38, "lugar_de_interes")]
+    )
+]
+nlp = spacy.load(LANGUAGE)
+
+
+def parse_tokens(processed_text: spacy.tokens.Doc) -> List[Dict[str, Union[str, bool]]]:
+    return [{
+        'text': token.text,
+        'lemma': token.lemma_,
+        'pos': token.pos_,
+        'tag': token.tag_,
+        'shape': token.shape_,
+        'stopword': token.is_stop,
+        'punctuation': token.is_punct,
+        'whitespace': token.is_space,
+        'ent_type': token.ent_type_
+    } for token in processed_text]
+
+
+def training_model(spacy_model: spacy.Language) -> None:
+    n_iterations = 10
+    for s_iter in range(n_iterations):
+        random.shuffle(TRAINING_TEXTS)
+        for raw_text, entity_positions in TRAINING_TEXTS:
+            doc = spacy_model.make_doc(raw_text)
+            example = Example.from_dict(doc, {"entities": entity_positions})
+            spacy_model.update([example])
+            # Para guardar el modelo en disco
+            # spacy_model.to_disk('RUTA_DEL_MODELO')
+
+
+tokens = nlp(TEXT)
+print("Modelo antes de ser entrenado\n")
+for t in parse_tokens(tokens):
+    print(t)
+
+training_model(spacy_model=nlp)
+tokens2 = nlp(TEXT)
+print("\nModelo después de ser entrenado\n")
+for t in parse_tokens(tokens2):
+    print(t)
